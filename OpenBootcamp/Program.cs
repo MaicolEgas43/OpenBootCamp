50726bd5720217db77cc087446ef30ffac177f11
MaicolEgas43 <maicolegas43@gmail.com>
2023-02-22 19:02:46 -0500

    First Module completed

---
+++ OpenBootcamp/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("What´s your full name?");
+var fullName = Console.ReadLine();
+Console.WriteLine("Your full name is: " + fullName);
---
+++ OpenBootcamp/WhatDate.cs
@@ -0,0 +1,10 @@
+﻿
+public class WhatDate
+{
+    private static void Main(string[] args)
+    {
+        var dateActual = DateTime.Now;
+
+        Console.WriteLine("La fecha actual es: " + dateActual.ToString("dd/MM/yyyy HH:mm:ss"));
+    }
+}

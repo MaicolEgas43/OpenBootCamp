9de7448065c610c8df817fd05068b749e4dd92f8
MaicolEgas43 <maicolegas43@gmail.com>
2023-02-22 23:49:57 -0500

    Exercise sesion 2 finisehd

---
+++ OpenBootcamp/Sesion_2/E2Mesa.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenBootcamp.Sesion_2
+{
+    public class E2Mesa
+    {
+        public int Peso { get; set; }
+        public int Largo { get; set; }
+        public string Material { get; set; }
+        public string Color { get; set; }
+    }
+}

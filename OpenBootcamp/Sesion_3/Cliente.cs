f2efc3e6febb22e15ca296c41b17920db1b2c873
MaicolEgas43 <maicolegas43@gmail.com>
2023-02-28 15:29:08 -0500

    New Brancha and exercise finished

---
+++ OpenBootcamp/Sesion_3/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenBootcamp.Sesion_3
+{
+    internal class Cliente
+    {
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public bool isNewClient { get; set; }
+
+    }
+}
---
+++ OpenBootcamp/Sesion_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenBootcamp.Sesion_3;
+
+namespace OpenBootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Cliente = new Cliente();
+
+            //Cliente.Nombre = Cliente.Nombre;
+            //Cliente.Telefono = Cliente.Telefono;
+            //Cliente.Email = Cliente.Email;
+            //Cliente.Direccion = Cliente.Direccion;
+            //Cliente.isNewClient = false;
+            Cliente.Nombre = "Maicol Estiben Egas";
+            Cliente.Telefono = "3158211221";
+            Cliente.Email = "maicolegas43@gmail.com";
+            Cliente.Direccion = "Cra 36a #94a - 43";
+            Cliente.isNewClient = true;
+
+
+            Console.WriteLine($"Los datos del cliente son: Nombre {Cliente.Nombre}, Telefono: {Cliente.Telefono}, Email {Cliente.Email}, Dirección {Cliente.Direccion}, y {(Cliente.isNewClient ? "No es nuevo" : "Es nuevo")} Cliente");
+        }
+
+        
+    }
+}
